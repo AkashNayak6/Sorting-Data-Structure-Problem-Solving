a774be73165009ce399bfdc113e96ec81e490e21
AkashNayak6 <46473508+AkashNayak6@users.noreply.github.com>
2019-06-15 23:47:30 -0400

    Create Problem2.java

---
+++ Problem2/Problem2.java
@@ -0,0 +1,47 @@
+public class Problem2 {
+		
+		public class node;		// pointer
+		
+		
+		node =head;			// creates pointer as head
+		head =null;			// head of null exception
+	
+
+			Problem2(int[] arr);
+			for (int i=0; i<array; i++)		// length of array
+				
+				
+			
+			Void Data(){			// constructor
+				int data;			// variable initiated
+				
+			}
+			
+			Void add(int[] a) {			// add function that adds an array "a" to data structure
+					
+					node = head;		// adds pointer node to head and adds to an array
+					temp[] = head;		//temp storage created for value of initial
+					head =head.next;	// pointer points to next
+					add (next);			// add next 
+					
+			}
+			
+			int getLargest(int k) {				// obtains kth smallest value and return it
+				
+				for (int k=0; k<array; k++)		// array loop to find largest 
+				{
+					
+					if (k > min)				// less than array size
+						k= min.value				// finds the min value associated from the main file calling from the function
+						System.out.println (int[] arr)
+				
+						return k;
+			}
+			
+	
+
+		}
+	
+
+	}		// add array {1,9,4,5,10,0} to structure in that order and calls getLargest(4) 
+
