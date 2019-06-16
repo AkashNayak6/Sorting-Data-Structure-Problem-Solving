3876dca00ff6ef2cc23f1a8766872b7d3e407f53
AkashNayak6 <46473508+AkashNayak6@users.noreply.github.com>
2019-06-15 23:48:40 -0400

    Create Problem3.java

---
+++ Problem3/Problem3.java
@@ -0,0 +1,25 @@
+public class Problem3 {
+	
+
+		int array;			// to store the array, size and min value  
+		int size;
+		int min;
+		int value;
+		array = size();
+		
+		
+		for (i=0; i<size; i++) {			// creates size
+			for (j=0; j>size; j++)
+		}
+			if (min <first.value)			// use for min value and max and creates the swaps between the arrays
+			swap (i,j)
+			else (min > first.value);
+			swap (i,j);
+			
+		for (int =size; size<array; size++){		.// print array 
+			System.out.println(int[] "a")
+		}
+			
+	} // example [8,9,0,3,1] for the min-max alternation and that it runs in O(N)time 
+
+
